45ccce34573bdde3ab4c8fa63f2286c3983836f3
hanv81 <nvha81@gmail.com>
2022-12-21 00:46:08 +0700

    similar finder

--- img_search.py
+++ img_search.py
@@ -1,74 +1,6 @@
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from img2vec_pytorch import Img2Vec
-from dataset import ImageDataset
 from duplicate_finder import DuplicateFinder
-from imutils import paths
-from PIL import Image
-from tqdm import tqdm
-import numpy as np
-import pickle
-import faiss
-import os
-import shutil
+from similar_finder import SimilarFinder
 import argparse
-import pandas as pd
-
-def load_image_vectors():
-    vectors = pickle.loads(open("vectors.pickle", "rb").read())
-    return vectors
-
-def create_image_vectors(image_path):
-    print('Creating image vectors')
-    i2v = Img2Vec()
-    to_pil = transforms.ToPILImage()
-    vectors = None
-    dataloader = DataLoader(ImageDataset(image_path), batch_size=32)
-    for images in tqdm(dataloader, desc='Processing batch'):
-        images = [to_pil(image) for image in images]
-        if vectors is None:
-            vectors = i2v.get_vec(images)
-        else:
-            vectors = np.concatenate((vectors, i2v.get_vec(images)), axis = 0)
-
-    f = open("vectors.pickle", "wb")
-    f.write(pickle.dumps(vectors))
-    f.close()
-    return vectors
-
-def create_faiss_index(vectors):
-    faiss.normalize_L2(vectors)
-    index = faiss.IndexFlatIP(vectors.shape[1])
-    index.add(vectors)
-    return index
-
-def search_similar(index, image_path, query_path):
-    img_paths = list(paths.list_images(image_path))
-    query_img_paths = list(paths.list_images(query_path))
-    images = [Image.open(path) for path in query_img_paths]
-    vectors = Img2Vec().get_vec(images)
-    faiss.normalize_L2(vectors)
-    k = 4
-    D, I = index.search(vectors, k)
-    data = {'query_image': [], 'similar_image': [], 'similarity': []}
-    for i in range(len(D)):
-        query_path = query_img_paths[i]
-        query_filename = os.path.basename(query_path)
-        if not os.path.exists(query_path + '_'):
-            os.mkdir(query_path + '_')
-        print(f'Similar image of {query_path}')
-        for j in range(k):
-            print(f'\t {img_paths[I[i][j]]} {D[i][j]}')
-            shutil.copy(img_paths[I[i][j]], query_path + '_')
-            if query_filename in data['query_image']:
-                data['query_image'].append('')
-            else:
-                data['query_image'].append(query_filename)
-            data['similar_image'].append(os.path.basename(img_paths[I[i][j]]))
-            data['similarity'].append(D[i][j])
-
-    df = pd.DataFrame(data)
-    df.to_csv('similar.csv')
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -87,13 +19,8 @@ def main():
         finder = DuplicateFinder(args.hash_size)
         finder.find_duplicate(args.image_path, args.batch_size)
     else:
-        if os.path.exists('vectors.pickle'):
-            vectors = load_image_vectors()
-        else:
-            vectors = create_image_vectors(args.image_path)
-
-        index = create_faiss_index(vectors)
-        search_similar(index, args.image_path, args.query_path)
+        finder = SimilarFinder(args.image_path, args.batch_size)
+        finder.find_similar(args.query_path)
 
 if __name__ == "__main__":
     main()
---
+++ similar_finder.py
@@ -0,0 +1,86 @@
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from img2vec_pytorch import Img2Vec
+from dataset import ImageDataset
+from imutils import paths
+from PIL import Image
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import pickle
+import faiss
+import os
+import shutil
+
+class SimilarController:
+    def __init__(self, image_path, batch_size):
+        self.image_path = image_path
+        self.i2v = Img2Vec()
+        self.dataloader = DataLoader(ImageDataset(image_path), batch_size=32)
+
+    def load_image_vectors(self):
+        vectors = pickle.loads(open("vectors.pickle", "rb").read())
+        return vectors
+
+    def create_image_vectors(self):
+        print('Creating image vectors')
+        to_pil = transforms.ToPILImage()
+        vectors = None
+        for images in tqdm(self.dataloader, desc='Processing batch'):
+            images = [to_pil(image) for image in images]
+            if vectors is None:
+                vectors = self.i2v.get_vec(images)
+            else:
+                vectors = np.concatenate((vectors, self.i2v.get_vec(images)), axis = 0)
+
+        f = open("vectors.pickle", "wb")
+        f.write(pickle.dumps(vectors))
+        f.close()
+        return vectors
+
+    def create_faiss_index(self):
+        if os.path.exists('vectors.pickle'):
+            vectors = self.load_image_vectors()
+        else:
+            vectors = self.create_image_vectors()
+
+        faiss.normalize_L2(vectors)
+        self.index = faiss.IndexFlatIP(vectors.shape[1])
+        self.index.add(vectors)
+
+    def find_similar(self, query_path, k = 4):
+        self.create_faiss_index()
+
+        img_paths = list(paths.list_images(self.image_path))
+        query_img_paths = list(paths.list_images(query_path))
+        images = [Image.open(path) for path in query_img_paths]
+        vectors = self.i2v.get_vec(images)
+        faiss.normalize_L2(vectors)
+        D, I = self.index.search(vectors, k)
+
+        data = {'query_image': [], 'similar_image': [], 'similarity': []}
+        for i in tqdm(range(len(D)), desc='Similar search result'):
+            query_path = query_img_paths[i]
+            query_filename = os.path.basename(query_path)
+            if not os.path.exists(query_path + '_'):
+                os.mkdir(query_path + '_')
+            # print(f'Similar image of {query_path}')
+            for j in range(k):
+                # print(f'\t {img_paths[I[i][j]]} {D[i][j]}')
+                shutil.copy(img_paths[I[i][j]], query_path + '_')
+                if query_filename in data['query_image']:
+                    data['query_image'].append('')
+                else:
+                    data['query_image'].append(query_filename)
+                data['similar_image'].append(os.path.basename(img_paths[I[i][j]]))
+                data['similarity'].append(D[i][j])
+
+        df = pd.DataFrame(data)
+        df.to_csv('similar.csv')
+
+class SimilarFinder:
+    def __init__(self, image_path, batch_size):
+        self.controller = SimilarController(image_path, batch_size)
+
+    def find_similar(self, query_path):
+        self.controller.find_similar(query_path)
