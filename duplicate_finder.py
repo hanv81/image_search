9123edccce54d3ce59beece93f966e0e9ac7ce4a
hanv81 <nvha81@gmail.com>
2022-12-21 23:57:33 +0700

    aggregate duplicate result

--- duplicate_finder.py
+++ duplicate_finder.py
@@ -3,8 +3,11 @@ import imagehash
 import pickle
 import shutil
 import time
+import pandas as pd
 from imutils import paths
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+from collections import deque
 from vptree import VPTree
 from PIL import Image
 
@@ -24,24 +27,24 @@ class HashController:
         self.tree = pickle.loads(open("vptree.pickle", "rb").read())
         self.hashes = pickle.loads(open("hashes.pickle", "rb").read())
 
-    def __batch_hashing(self, file_paths):
-        for path in file_paths:
-            h = self.hasher.get_hash(path)
+    def __batch_hashing(self, file_paths, fr, to):
+        to = min(len(file_paths), to)
+        for i in range(fr, to):
+            h = self.hasher.get_hash(file_paths[i])
             p = self.hashes.get(h, [])
-            p.append(path)
+            p.append(i)
             self.hashes[h] = p
 
     def __create_hashes(self, path, batch_size):
-        print("Hashing images...")
-        t = time.time()
+        # t = time.time()
         self.hashes = {}
         image_paths = list(paths.list_images(path))
         with ThreadPoolExecutor() as executor:
-            for i in range(0, len(image_paths), batch_size):
-                executor.submit(self.__batch_hashing, image_paths[i : i + batch_size])
+            for i in tqdm(range(0, len(image_paths), batch_size), desc="Hashing images..."):
+                executor.submit(self.__batch_hashing, image_paths, i, i + batch_size)
         
-        t = int(time.time()-t)
-        print(t)
+        # t = int(time.time()-t)
+        # print(t)
         f = open("hashes.pickle", "wb")
         f.write(pickle.dumps(self.hashes))
         f.close()
@@ -58,7 +61,7 @@ class HashController:
         f.write(pickle.dumps(self.tree))
         f.close()
 
-    def search_duplicate(self, path, batch_size):
+    def _search_duplicate(self, path, batch_size):
         if os.path.exists('vptree.pickle'):
             self.__load_hashes_and_vptree()
         else:
@@ -67,18 +70,56 @@ class HashController:
 
         if not os.path.exists('duplicate'):
             os.mkdir('duplicate')
-        for h in self.hashes.keys():
-            results = self.tree.get_all_in_range(h, 4)
-            if len(results) > 1:
-                duplicate_path = os.path.join('duplicate', os.path.basename(self.hashes[h][0]))
-                if not os.path.exists(duplicate_path):
-                    os.mkdir(duplicate_path)
-                shutil.copy(self.hashes[h][0], duplicate_path)
-                print(f'{self.hashes[h]} have duplicate:')
-                for d, hh in results:
-                    if d > 0:
-                        print(f'\t{self.hashes[hh]} {d}')
-                        shutil.copy(self.hashes[hh][0], duplicate_path)
+
+        result = {}
+        for h in tqdm(self.hashes.keys(), desc='Searching duplicate ...'):
+            neighbors = self.tree.get_all_in_range(h, 4)
+            if len(neighbors) > 1:
+                i = self.hashes[h][0]
+                result[i] = self.hashes[h][1:]
+
+                for _, hh in neighbors:
+                    for j in self.hashes[hh]:
+                        if j not in self.hashes[h]:
+                            result[i].append(j)
+
+        return result
+
+    def _aggregate_result(self, input):
+        result = []
+        visited = set()
+        for key in tqdm(input, desc='Aggregating result ...'):
+            if key in visited:
+                continue
+
+            queue = deque([key])
+            reachable = set()
+
+            while queue:
+                curr_key = queue.popleft()
+                reachable.add(curr_key)
+                for value in input[curr_key]:
+                    if value not in visited:
+                        queue.append(value)
+                visited.add(curr_key)
+
+            result.append(reachable)
+
+        return result
+
+    def _export_excel(self, result, path):
+        image_paths = list(paths.list_images(path))
+        data = {'image': [], 'duplicate_image': []}
+        for s in result:
+            s = list(s)
+            data['image'].append(os.path.basename(image_paths[s[0]]))
+            for i in range(1, len(s)):
+                if i > 1:
+                    data['image'].append('')
+                data['duplicate_image'].append(os.path.basename(image_paths[s[i]]))
+
+        df = pd.DataFrame(data)
+        df.to_csv('duplicate.csv')
 
 class DuplicateFinder:
     def __init__(self, hash_size):
@@ -86,10 +127,6 @@ class DuplicateFinder:
         self.hash_controller = HashController(hasher)
 
     def find_duplicate(self, path, batch_size):
-        self.hash_controller.search_duplicate(path, batch_size)
-
-    def _aggregate_result(self):
-        pass
-
-    def _export_excel(self):
-        pass
+        result = self.hash_controller._search_duplicate(path, batch_size)
+        result = self.hash_controller._aggregate_result(result)
+        self.hash_controller._export_excel(result, path)
